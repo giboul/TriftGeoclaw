527163b35638a4cf7332a1fa5fd315ab6a4430b1
giboul <axel.giboulot@epfl.ch>
2024-06-10 15:18:28 +0200

    timing legend

--- Flume/reading.py
+++ Flume/reading.py
@@ -25,17 +25,18 @@ hn[x < 10] = (10/50/np.sqrt(0.05))**(2/3)
 fig, ax = plt.subplots(figsize=(5, 2.7), layout="tight")
 ax.fill_between(x, hn+s, s, alpha=0.1, label='normal depth')
 ax.fill_between(x, s, s.min(), color="k")
-ax.legend(loc='upper right')
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$h(x, t)$')
 
 # Initialize water line to plot and animate
-line, = ax.plot(x, frame.state.q[0]+s, label=rf"$ t = {frame.t}$ s")
+line, = ax.plot(x, frame.state.q[0]+s)
 # Function for water line udpating
 def update(i):
     frame = solution.Solution(i, path=dir, file_format='ascii')
     h  = frame.state.q[0]
     line.set_data(x, h+s)
+    line.set_label(rf"$ t = {frame.t}$ s")
+    ax.legend(loc='upper right')
     fig.canvas.draw()
 # Animate
 anim = FuncAnimation(fig, update, frames=len(files), interval=500/len(files))
