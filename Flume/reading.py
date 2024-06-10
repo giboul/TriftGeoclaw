ebc131cfc1a4bcca65d978925e6314530fee50f5
giboul <axel.giboulot@epfl.ch>
2024-06-10 15:38:52 +0200

    clarify

--- Flume/reading.py
+++ Flume/reading.py
@@ -9,8 +9,6 @@ dir = '_output'
 files = glob(f'{dir}/fort.q*')
 print(f"There are {len(files)} files in {dir}")
 
-# %%
-
 # Get first frame
 frame = solution.Solution(0, path=dir, file_format='ascii')
 # Get bed arrays
@@ -23,13 +21,14 @@ hn[x < 10] = (10/50/np.sqrt(0.05))**(2/3)
 
 # Initialize figure
 fig, ax = plt.subplots(figsize=(5, 2.7), layout="tight")
-ax.fill_between(x, hn+s, s, alpha=0.1, label='normal depth')
-ax.fill_between(x, s, s.min(), color="k")
+# Initialize water line to plot and animate
+line, = ax.plot(x, frame.state.q[0]+s)
+# Bed and normal depth
+ax.fill_between(x, hn+s, s, alpha=0.1, label='Normal depth')
+ax.fill_between(x, s, s.min(), color="k", label="Bed")
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$h(x, t)$')
 
-# Initialize water line to plot and animate
-line, = ax.plot(x, frame.state.q[0]+s)
 # Function for water line udpating
 def update(i):
     frame = solution.Solution(i, path=dir, file_format='ascii')
@@ -40,5 +39,5 @@ def update(i):
     fig.canvas.draw()
 # Animate
 anim = FuncAnimation(fig, update, frames=len(files), interval=500/len(files))
-# anim.save("movie.gif", fps=len(files)/5)
 plt.show()
+# anim.save("movie.gif", fps=len(files)/5)
