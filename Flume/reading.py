2a3095500bdadd223de2efa76684b25b493419c8
giboul <axel.giboulot@epfl.ch>
2024-06-11 09:39:47 +0200

    details

--- Flume/reading.py
+++ Flume/reading.py
@@ -18,6 +18,7 @@ s = frame.state.aux[0]
 # Compute normal depth (Chézy)
 hn = np.full_like(x, (10/50/np.sqrt(0.002))**(2/3))
 hn[x < 10] = (10/50/np.sqrt(0.05))**(2/3)
+hn[x > 1000] = 1.
 
 # Initialize figure
 fig, ax = plt.subplots(figsize=(5, 2.7), layout="tight")
@@ -25,7 +26,7 @@ fig, ax = plt.subplots(figsize=(5, 2.7), layout="tight")
 line, = ax.plot(x, frame.state.q[0]+s)
 # Bed and normal depth
 ax.fill_between(x, hn+s, s, alpha=0.1, label='Normal depth')
-ax.fill_between(x, s, s.min(), color="k", label="Bed")
+ax.fill_between(x, s, s.min()-.5, color="k", label="Bed")
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$h(x, t)$')
 
@@ -34,10 +35,12 @@ def update(i):
     frame = solution.Solution(i, path=dir, file_format='ascii')
     h  = frame.state.q[0]
     line.set_data(x, h+s)
-    line.set_label(rf"$ t = {frame.t}$ s")
+    line.set_label(rf"$ t = {frame.t:.1f}$ s")
     ax.legend(loc='upper right')
     fig.canvas.draw()
 # Animate
 anim = FuncAnimation(fig, update, frames=len(files), interval=500/len(files))
-plt.show()
-# anim.save("movie.gif", fps=len(files)/5)
+if False:
+    plt.show()
+else:
+    anim.save("movie.gif", fps=len(files)/5, dpi=500)
