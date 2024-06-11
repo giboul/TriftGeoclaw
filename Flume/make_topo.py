db7e57f1d8442bd7694542fdab39779d77692374
giboul <axel.giboulot@epfl.ch>
2024-06-11 09:39:13 +0200

    with lake and right-wall

--- Flume/make_topo.py
+++ Flume/make_topo.py
@@ -1,27 +1,27 @@
+from scipy.interpolate import interp1d
 import numpy as np
 
 xlower = 0
-xupper = 1000.
+xupper = 3000.
 
-slope1  = 0.05
-slope2  = 0.002
-mx     = 1000
+slope1 = 0.05
+slope2 = 0.002
+slope3 = 0.
+mx     = 1000 + 1
 x_A    = 10
+x_B    = 1000.
 z_A    = 0-slope1*x_A
+z_B    = z_A -(x_B-x_A)*slope2
 
-x = np.linspace(xlower, xupper, mx+1)
-z = 0-slope1*x
+x = np.linspace(xlower, xupper, num=mx, endpoint=True)
+z = interp1d((xlower, x_A, x_B, xupper), (0, z_A, z_B, z_B))(x)
 
-for i in range(mx+1):
-	if x[i] > x_A:
-		z[i] = z_A-slope2*x[i]
- 
+if False:
+    from matplotlib import pyplot as plt
+    plt.fill_between(x, z, z.min()-1)
+    plt.show()
 
-fname='topography.data'
-
-with open(fname,'w') as f:
-        f.write('%i   # number of points\n' % (mx+1))
-
-        for i in range(mx+1):
-            f.write('%15.8f %15.8f\n' % (x[i], z[i]))
-        f.close()
+np.savetxt(fname="topography.data",
+           X=np.vstack((x, z)).T,
+           header=f"{mx} # number of points",
+           comments="")
